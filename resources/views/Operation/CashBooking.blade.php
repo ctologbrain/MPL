e5b122b26c2d777f88e9fc493736e6bf25739d64
root <sachin@logbrains.com>
2023-02-21 17:32:41 +0530

    sidebar again editting

--- resources/views/Operation/CashBooking.blade.php
+++ resources/views/Operation/CashBooking.blade.php
@@ -1034,6 +1034,21 @@ function submitAllData(){
     alert('Please Enter Consignee Address');
     return false;
  }
+ if( $("#CoAddress").val()=='')
+ {
+    alert('Please Enter Consignee Address');
+    return false;
+ }
+ if( $("#TrafReceivedAmount").val()=='')
+ {
+    alert('Please Enter Received Amount');
+    return false;
+ }
+ if( $("#PaymentMethod").val()=='')
+ {
+    alert('Please Select Payment Method');
+    return false;
+ }
  if( $("#Product").val()=='')
  {
     alert('Please Enter Product');
