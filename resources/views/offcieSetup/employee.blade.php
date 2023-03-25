34b44bc8deafed34fb80afde534cc09057becb52
root <sachin@logbrains.com>
2023-03-25 18:32:24 +0530

    sidebar again editting

--- resources/views/offcieSetup/employee.blade.php
+++ resources/views/offcieSetup/employee.blade.php
@@ -444,7 +444,7 @@
             @endforeach
          </tbody>
         </table>
-        {!! $employeeDetailsgit->appends(Request::all())->links() !!}
+        {!! $employeeDetails->appends(Request::all())->links() !!}
 </div>
            </div>
          </div>
