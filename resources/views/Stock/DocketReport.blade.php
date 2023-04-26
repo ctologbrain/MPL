00026b12a91ada6f2cb62699283f704664b98f82
ctologbrain <sachin@logbrains.com>
2023-04-26 20:07:11 +0530

    changecode

--- resources/views/Stock/DocketReport.blade.php
+++ resources/views/Stock/DocketReport.blade.php
@@ -43,10 +43,10 @@
                    </select>
                    </div>
                    <div class="mb-2 col-md-2">
-                   <input @if(request()->get('formDate')!='')  value="{{request()->get('formDate')}}" @else value="{{date("Y-m-d")}}" @endif type="text" name="formDate" class="form-control datepickerOne" placeholder="From Date" autocomplete="off">
+                   <input @if(request()->get('formDate')!='')  value="{{request()->get('formDate')}}"  @endif type="text" name="formDate" class="form-control datepickerOne" placeholder="From Date" autocomplete="off">
                    </div>
                    <div class="mb-2 col-md-2">
-                   <input @if(request()->get('todate')!='')    value="{{request()->get('todate')}}" @else value="{{date("Y-m-d")}}" @endif type="text" name="todate" class="form-control datepickerOne" placeholder="To Date" autocomplete="off">
+                   <input @if(request()->get('todate')!='')    value="{{request()->get('todate')}}"  @endif type="text" name="todate" class="form-control datepickerOne" placeholder="To Date" autocomplete="off">
                    </div>
                    
                    <div class="mb-2 col-md-2">
