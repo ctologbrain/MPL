396599696d971443eb4a9aa7b1a66982ad5f06a9
root <sachin@logbrains.com>
2023-04-26 15:48:59 +0530

    Change Sum

--- app/Models/Account/InvoiceDetails.php
+++ app/Models/Account/InvoiceDetails.php
@@ -5,9 +5,8 @@ namespace App\Models\Account;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class CustomerInvoice extends Model
+class InvoiceDetails extends Model
 {
     use HasFactory;
-    protected $table="InvoiceMaster";
-
+    protected $table="InvoiceDetails";
 }
