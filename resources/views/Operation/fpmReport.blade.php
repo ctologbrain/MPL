b376137934d96d294d79ed434c68e2575fc7d50b
ctologbrain <sachin@logbrains.com>
2023-04-27 18:59:48 +0530

    changecode

--- resources/views/Operation/fpmReport.blade.php
+++ resources/views/Operation/fpmReport.blade.php
@@ -58,7 +58,16 @@
            </tr>
          </thead>
          <tbody>
-            <?php $i=0; ?>
+            <?php $i=0; 
+            $page=request()->get('page');
+            if(isset($page) && $page>1){
+                $page =$page-1;
+            $i = intval($page*10);
+            }
+             else{
+            $i=0;
+            }
+            ?>
             @foreach($data as $Fpmdata)
              <?php 
              $i++; ?>
