53acf79383daef82f308145424e68bd07bcae2d8
ctologbrain <sachin@logbrains.com>
2023-05-10 13:52:52 +0530

    changecode

--- app/Http/Controllers/Account/CustomerTariffController.php
+++ app/Http/Controllers/Account/CustomerTariffController.php
@@ -219,7 +219,7 @@ class CustomerTariffController extends Controller
         {
             $city=city::get(); 
             $html='';
-            $html.='<option value="">--select--</option>';
+            
             foreach($city as $cityLsit)
             {
               $html.='<option value="'.$cityLsit->id.'">'.$cityLsit->Code.'~'.$cityLsit->CityName.'</option>';
@@ -231,7 +231,7 @@ class CustomerTariffController extends Controller
         {
             $state=state::get(); 
             $html='';
-            $html.='<option value="">--select--</option>';
+            
             foreach($state as $StateLsit)
             {
               $html.='<option value="'.$StateLsit->id.'">'.$StateLsit->name.'</option>';
@@ -242,7 +242,7 @@ class CustomerTariffController extends Controller
         {
             $zone=ZoneMaster::get(); 
             $html='';
-            $html.='<option value="">--select--</option>';
+           
             foreach($zone as $ZoneLsit)
             {
               $html.='<option value="'.$ZoneLsit->id.'">'.$ZoneLsit->ZoneName.'</option>';
@@ -253,7 +253,7 @@ class CustomerTariffController extends Controller
         {
             $Pincode=PincodeMaster::with('StateDetails','CityDetails')->get(); 
             $html='';
-            $html.='<option value="">--select--</option>';
+           
             foreach($Pincode as $PinLsit)
             {
                 if(isset($PinLsit->CityDetails->CityName))
--- resources/views/CompanySetup/Bankdetails.blade.php
+++ resources/views/CompanySetup/Bankdetails.blade.php
@@ -204,7 +204,7 @@
 <script src="https://code.jquery.com/jquery-3.6.0.js"></script>
 <script src="https://code.jquery.com/ui/1.13.1/jquery-ui.js"></script>
 <script type="text/javascript">
-$(".Selectbox").select2();
+$(".Selectbox").Select2();
 $('.datepickerOne').datepicker({
     dateFormat: 'yy-mm-dd'
 });
