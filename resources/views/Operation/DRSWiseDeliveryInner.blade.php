70c472c84227c5676319d3a68cdf78c6df6c8b36
ctologbrain <sachin@logbrains.com>
2023-05-12 15:35:59 +0530

    changecode

--- resources/views/Operation/DRSWiseDeliveryInner.blade.php
+++ resources/views/Operation/DRSWiseDeliveryInner.blade.php
@@ -25,7 +25,9 @@
     <tbody>
     <?php $i=0; ?>
         @foreach($docketData as $Docket)
-        <?php $i++; ?>
+        <?php $i++; 
+        $tab =$i+6;
+        ?>
         <tr>
         <td>{{$i}}</td>
         <td><input type="text" class="form-control awb_number" name="docket[{{$i}}][docket]" id="awb_number" tabindex="5" value="{{$Docket->Docket_No}}"></td>
@@ -40,24 +42,24 @@
                 </select>
             </td>
             <td><input type="text" class="form-control actual_pieces" name="docket[{{$i}}][actual_pieces]" id="actual_pieces"
-                    tabindex="7" value="{{$Docket->pieces}}"></td>
+                    tabindex="{{$tab}}" value="{{$Docket->pieces}}"></td>
             <td><input type="text" class="form-control delievery_pieces" name="docket[{{$i}}][delievery_pieces]" id="delievery_pieces"
-                    tabindex="8"></td>
-            <td><input type="text" class="form-control weight" name="docket[{{$i}}][weight]" id="weight" tabindex="9"></td>
-            <td><input type="text" class="form-control time datepickerOne" name="docket[{{$i}}][time]" id="time" tabindex="10"></td>
-            <td><select name="docket[{{$i}}][proof_name]" tabindex="11" class="form-control selectBox type" id="type">
+                    tabindex="{{$tab}}"></td>
+            <td><input type="text" class="form-control weight" name="docket[{{$i}}][weight]" id="weight" tabindex="{{$tab}}"></td>
+            <td><input type="text" class="form-control time datepickerOne" name="docket[{{$i}}][time]" id="time" tabindex="{{$tab}}"></td>
+            <td><select name="docket[{{$i}}][proof_name]" tabindex="{{$tab}}" class="form-control selectBox type" id="type">
                     <option value="">--select--</option>
                     @foreach($dproof as $proof)
                     <option value="{{$proof->id}}">{{$proof->ProofName}}</option>
                     @endforeach
                   </select></td>
             <td><input type="text" class="form-control reciever_name" name="docket[{{$i}}][reciever_name]" id="reciever_name"
-                    tabindex="12"></td>
-            <td><input type="number" class="form-control phone" name="docket[{{$i}}][phone]" id="phone" tabindex="13"></td>
+                    tabindex="{{$tab}}"></td>
+            <td><input type="number" class="form-control phone" name="docket[{{$i}}][phone]" id="phone" tabindex="{{$tab}}"></td>
             <td><input type="text" class="form-control proof_detail" name="docket[{{$i}}][proof_detail]" id="proof_detail"
-                    tabindex="14"></td>
+                    tabindex="{{$tab}}"></td>
             <td>
-                <select disabled name="docket[{{$i}}][ndr_reason]" tabindex="15" class="form-control selectBox ndr_reason" id="ndr_reason{{$i}}">
+                <select disabled name="docket[{{$i}}][ndr_reason]" tabindex="{{$tab}}" class="form-control selectBox ndr_reason" id="ndr_reason{{$i}}">
                     <option value="">--select--</option>
                      @foreach($ndr as $ndrMaster)
                     <option value="{{$ndrMaster->id}}">{{$ndrMaster->ReasonDetail}}</option>
@@ -67,7 +69,7 @@
 
                 </select>
             </td>
-            <td><input readonly type="text" class="form-control ndr_remark" name="docket[{{$i}}][ndr_remark]" id="ndr_remark{{$i}}" tabindex="16"></td>
+            <td><input readonly type="text" class="form-control ndr_remark" name="docket[{{$i}}][ndr_remark]" id="ndr_remark{{$i}}" tabindex="{{$tab}}"></td>
 
         </tr>
        
