29eac8d2fdff8bc9ea2a3581ea31e638931983fa
ctologbrain <sachin@logbrains.com>
2023-05-15 15:46:52 +0530

    changecode

--- resources/views/Operation/ndrReportList.blade.php
+++ resources/views/Operation/ndrReportList.blade.php
@@ -48,7 +48,7 @@
                                                   </select>
                                                 </div>
                                                 <div class="col-3">
-                                                  <button type="button" class="btn btn-primary" tabindex="2">Total Record: 11</button>
+                                                  <button type="button" class="btn btn-primary" tabindex="2">Total Record: {{$NdrReport->total()}}</button>
                                                 </div>
                                                 </div>
                                               </div>
@@ -149,20 +149,7 @@
                                          </div>
                                          <div class="col-md-12">
                                             <div class="d-flex d-flex justify-content-between">
-                                             <nav>
-                                                <ul class="pagination">
-                                                    
-                                                        <li class="page-item disabled" aria-disabled="true" aria-label="« Previous">
-                                                            <span class="page-link" aria-hidden="true">‹</span>
-                                                        </li>
-                                                        <li class="page-item active" aria-current="page"><span class="page-link">1</span></li>
-                                                        <li class="page-item"><a class="page-link" href="http://localhost/MPL/docketbookingReport?page=2">2</a></li>
-                                                       <li class="page-item"><a class="page-link" href="http://localhost/MPL/docketbookingReport?page=3">3</a></li>
-                                                        <li class="page-item">
-                                                            <a class="page-link" href="http://localhost/MPL/docketbookingReport?page=2" rel="next" aria-label="Next »">›</a>
-                                                        </li>
-                                                </ul>
-                                             </nav>
+                                            {!! $NdrReport->appends(Request::all())->links() !!}
 
                                             </div>
                                          </div>
