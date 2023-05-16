95d53b0f7d6e3d487fe3adfd609e83754a6457b1
ctologbrain <sachin@logbrains.com>
2023-05-16 15:05:35 +0530

    changecode

--- resources/views/Operation/topayDepositReport.blade.php
+++ resources/views/Operation/topayDepositReport.blade.php
@@ -112,7 +112,7 @@
              <td class="p-1">@isset($key->Branch){{$key->Branch}}  @endisset</td>
              <td class="p-1">@isset($key->Remark){{$key->Remark}}  @endisset</td>
              <td class="p-1">@if(isset($key->Attachment) && $key->Attachment!='') <a target="_blank" href="{{url($key->Attachment)}}" class="btn btn-primary p-1">View</a> @else
-             <a  href="javascript:void(0);" class="btn btn-primary p-1">View</a> 
+             <button disabled class="btn btn-primary p-1">No File</button> 
               @endif </td>
              
            </tr>
