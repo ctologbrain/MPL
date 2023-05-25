80e6285f673a969ceb1d4f0b95af4f45746e593c
ctologbrain <sachin@logbrains.com>
2023-05-25 18:30:56 +0530

    changecode

--- app/Http/Controllers/Operation/DRSEntryController.php
+++ app/Http/Controllers/Operation/DRSEntryController.php
@@ -203,7 +203,7 @@ class DRSEntryController extends Controller
           $query->where('DRS_Masters.D_Office_Id','=',$office);
         }
     })
-    ->groupby('docket_allocations.Docket_No','DRS_Masters.ID')
+    ->groupby('DRS_Masters.ID')
     ->orderby("DRS_Masters.ID","ASC")
     ->paginate(10);
    // echo '<pre>'; print_r( $DsrData[1]->getDRSTransDett ); die;
