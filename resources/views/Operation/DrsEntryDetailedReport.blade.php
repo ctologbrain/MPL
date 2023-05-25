7289138cf7906428051ef250dc227a7ff7910b00
ctologbrain <sachin@logbrains.com>
2023-05-25 17:00:25 +0530

    changecode

--- resources/views/Operation/DrsEntryDetailedReport.blade.php
+++ resources/views/Operation/DrsEntryDetailedReport.blade.php
@@ -87,7 +87,7 @@
             <tr>
              <td class="p-1">{{$i}}</td>
              <td class="p-1">@isset($key->DRSDocketDataDeatils->Docket_No) <a target="_blank" href="{{url('docketTracking?docket=').$key->DRSDocketDataDeatils->Docket_No}}"> {{$key->DRSDocketDataDeatils->Docket_No}} @endisset</td>
-             <td class="p-1"> {{$key->DRSDatasDetails->DRS_No}}</td>
+             <td class="p-1"><a href="{{url('PrintDRSEntry').'/'.$key->DRSDatasDetails->DRS_No}}" target="_blank"> {{$key->DRSDatasDetails->DRS_No}} </a></td>
              <td  class="p-1">{{$key->DRSDatasDetails->GetOfficeCodeNameDett->OfficeCode}}~{{$key->DRSDatasDetails->GetOfficeCodeNameDett->OfficeName}}</td>
              <td  class="p-1">@isset($key->DRSDatasDetails->Delivery_Date) {{date('d-m-Y', strtotime($key->DRSDatasDetails->Delivery_Date))}} @endisset </td>
             
