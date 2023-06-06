608dd0ddedcf751861612d0e4d8e3dd354d639fe
root <sachin@logbrains.com>
2023-06-06 16:46:42 +0530

    ssskl

--- app/Http/Controllers/Operation/GatePassReceivingController.php
+++ app/Http/Controllers/Operation/GatePassReceivingController.php
@@ -54,10 +54,11 @@ class GatePassReceivingController extends Controller
                 // if(empty($check)){
                 foreach($gatePassDetails->getPassDocketDetails as $Dockets)
                 {
-                
+                if($Dockets->getAllocationDetail->Status==5)
+                {
                 $i++;
                 $html.='<tr><td><input type="checkbox" class="docketFirstCheck" name="Docket['.$i.'][check]" value="'.$Dockets->Docket.'" id="check'.$Dockets->Docket.'"></td><td>'.$Dockets->Docket.'<input type="hidden" name="Docket['.$i.'][DocketNumber]" value="'.$Dockets->Docket.'"></td><td>'.$Dockets->pieces.'<input type="hidden" name="Docket['.$i.'][pices]" value="'.$Dockets->pieces.'"></td><td><input typ="text" class="form-control" id="receivedQty'.$Dockets->Docket.'" name="Docket['.$i.'][receivedQty]" onchange="getReceivedQty('.$Dockets->pieces.',this.value,'.$Dockets->Docket.','.$i.')"></td><td><input type="checkbox" id="ShotBox'.$Dockets->Docket.'" name="Docket['.$i.'][shotBox]"></td><td><input type="checkbox" id="ShotQty'.$i.'" name="Docket['.$i.'][ShotQty]"></td></tr>';    
-                
+                 }
                 }
 
                 $datas=array('status'=>'true','message'=>'success','datas'=>$gatePassDetails,'table'=>$html);
