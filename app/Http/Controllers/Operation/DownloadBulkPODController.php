2ee2af773cec15065eab10fd53f42c47910c6790
root <sachin@logbrains.com>
2023-06-16 14:53:10 +0530

    ddvds

--- app/Http/Controllers/Operation/DownloadBulkPODController.php
+++ app/Http/Controllers/Operation/DownloadBulkPODController.php
@@ -188,8 +188,8 @@ class DownloadBulkPODController extends Controller
             
                foreach ($docketImage as $key => $value) {
                  $file = basename($value->file);
-                 echo $newFile=public_path('document/'.basename($value->file));
-                 die;
+                 $newFile=public_path('document/'.basename($value->file));
+             
                  $zip->addFile($newFile, $file);
             }
              
