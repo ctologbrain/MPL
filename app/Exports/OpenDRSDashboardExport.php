5885724b49454f2fb9a8ac8aa8e8d104a55b28a9
ctologbrain <sachin@logbrains.com>
2023-07-01 18:56:58 +0530

    changecode

--- app/Exports/OpenDRSDashboardExport.php
+++ app/Exports/OpenDRSDashboardExport.php
@@ -27,7 +27,7 @@ class OpenDRSDashboardExport implements FromCollection, WithHeadings, ShouldAuto
        ->leftjoin('docket_allocations','docket_allocations.Docket_No','=','docket_masters.Docket_No')
        ->where("docket_allocations.Status","=",7)
        ->select( DB::raw("DATE_FORMAT(DRS_Masters.Delivery_Date,'%d-%m-%Y') as dattes)"),
-       DB::raw("CONCAT(office_masters.OfficeCode,'~',office_masters.OfficeName) as Offc"),
+    //   DB::raw("CONCAT(office_masters.OfficeCode,'~',office_masters.OfficeName) as Offc"),
        "DRS_Masters.DRS_No"  ,DB::raw("CONCAT(employees.EmployeeCode,'',employees.EmployeeName) as emp")
        ,"DRS_Transactions.Docket_No",DB::raw("CONCAT(DRS_Masters.DriverName, '(' , DRS_Masters.Mob ,')' ) as Drver"),
        "DRS_Masters.Vehcile_Type",
@@ -39,7 +39,7 @@ class OpenDRSDashboardExport implements FromCollection, WithHeadings, ShouldAuto
     {
         return [
             'DRS DATE',
-            'Delivery  Office',
+           // 'Delivery  Office',
             'Manifest Number',
             'Delivery Boy',
             'Docket No',
