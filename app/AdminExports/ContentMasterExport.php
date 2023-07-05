e4477aabb87c7b1c2cc822c3ca8ee97612cd9cfe
ctologbrain <sachin@logbrains.com>
2023-07-05 17:30:47 +0530

    changecode

--- app/AdminExports/ContentMasterExport.php
+++ app/AdminExports/ContentMasterExport.php
@@ -28,8 +28,8 @@ class ContentMasterExport implements FromCollection, WithHeadings, ShouldAutoSiz
         })
         ->select(
         'Contents',
-        DB::raw("(CASE WHEN Mode=1 THEN 'ALL'   WHEN Mode=1 THEN 'AIR' 
-        WHEN Mode=2 THEN 'COURIER'  WHEN Mode=3 THEN 'ROAD'  WHEN Mode=4 THEN 'TRAIN' END ) as md "),
+        DB::raw("(CASE WHEN Mode=1 THEN 'ALL'   WHEN Mode=2 THEN 'AIR' 
+        WHEN Mode=3 THEN 'COURIER'  WHEN Mode=4 THEN 'ROAD'  WHEN Mode=5 THEN 'TRAIN' END ) as md "),
         DB::raw("(CASE WHEN Active=1 THEN 'YES' ELSE  'NO' END) as ActiveStatus"),
         'users.name',
         'users.Created_At'
