a25b00b34a554e8beb3e8b8f81844e4ed1b06afe
ctologbrain <sachin@logbrains.com>
2023-07-24 12:37:41 +0530

    changecode

--- resources/views/Role/rolewisepermissionView.blade.php
+++ resources/views/Role/rolewisepermissionView.blade.php
@@ -150,7 +150,7 @@ $('#checkAllsecound').click(function () {
   table = document.getElementById("myTable");
   tr = table.getElementsByTagName("tr");
   for (i = 0; i < tr.length; i++) {
-    td = tr[i].getElementsByTagName("td")[0];
+    td = tr[i].getElementsByTagName("td")[2];
     if (td) {
       txtValue = td.textContent || td.innerText;
       if (txtValue.toUpperCase().indexOf(filter) > -1) {
