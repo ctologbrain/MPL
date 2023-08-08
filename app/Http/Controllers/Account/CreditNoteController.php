b26fbfd5ea55485add61599ce671b175b09a5340
ctologbrain <sachin@logbrains.com>
2023-08-08 13:39:52 +0530

    changecode

--- app/Http/Controllers/Account/CreditNoteController.php
+++ app/Http/Controllers/Account/CreditNoteController.php
@@ -181,7 +181,8 @@ class CreditNoteController extends Controller
     if(!empty( $data)){
        $pdf = PDF::loadView('Account.printCreditNote',["data" => $data]);
        $path = public_path('pdf/');
-       $id =end( explode("/", $CreditNo));
+       $crExp = explode("/", $CreditNo);
+       $id =end($crExp);
        $fileName = 'CRN23-24'.$id .'.' . 'pdf' ;
        $pdf->save($path . $fileName);
        return response()->file($path.$fileName);
